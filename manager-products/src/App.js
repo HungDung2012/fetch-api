ee1d72fd1cbccdb74b2280548c97e6fe974b6790
dung2012 <dungchuhung@gmail.com>
2025-01-19 14:18:28 +0700

    1

--- manager-products/src/App.js
+++ manager-products/src/App.js
@@ -8,7 +8,11 @@ function App() {
   return (
     <>
       <div>
-        this is Product and App
+        Với thuật toán A*, có thể sử dụng phiên bản Graph-search (tức là có danh sách nút
+        đóng) hoặc không. Trong trường hợp không sử dụng nút đóng, tất cả các nút mới sinh ra đều
+        được thêm vào tập biên. Trong trường hợp sử dụng phiên bản Graph-search, một nút đã mở
+        rộng sẽ không được thêm vào danh sách biên. Nếu một nút đã có một bản sao trong tập biên
+        thì bản sao với giá trị hàm f(n) nhỏ hơn sẽ được giữ lại.
       </div>
       <Product />
     </>
